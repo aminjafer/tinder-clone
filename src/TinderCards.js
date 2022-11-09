058c65556955a973b30a90eb0fda4b098daac883
aminjafer <amin.jafer@hotmail.com>
2022-11-09 21:14:21 +0500

    A working prototype connected to MongoDB cloud

--- src/TinderCards.js
+++ src/TinderCards.js
@@ -1,9 +1,22 @@
 import React, { useState } from 'react';
+import { useEffect } from 'react';
 import TinderCard from "react-tinder-card";
 import "./TinderCards.css";
+import axios from './axios';
 
 function TinderCards() {
     const [people, setPeople] = useState([]);
+
+    useEffect( () => {
+        async function fetchData() {
+            const req = await axios.get('/tinder/cards');
+            setPeople(req.data);            
+        }
+
+        fetchData();
+    }, []);
+
+    console.log(people);
 
     const swiped = (direction, nameToDelete) => {
         console.log("removing: " + nameToDelete);
@@ -25,7 +38,7 @@ function TinderCards() {
                     onCardLeftScreen = {() => outOfFrame(person.name)}
                 >
                     <div 
-                        style={ {backgroundImage: `url( ${person.url} )` }}
+                        style={ {backgroundImage: `url( ${person.imgUrl} )` }}
                         className="card"
                     >
                         <h3>{person.name}</h3>
